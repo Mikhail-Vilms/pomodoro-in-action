29ce5b95ddbfb7b6a346382eaf0602293bd68c0b
Mikhail-Vilms <mikhail.vilms@gmail.com>
2020-03-01 17:17:58 -0600

    FEAT: JWT token generation has been added

--- PomodoroInAction/Controllers/AppUsersController.cs
+++ PomodoroInAction/Controllers/AppUsersController.cs
@@ -1,7 +1,12 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
 using PomodoroInAction.Models;
 using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace PomodoroInAction.Controllers
@@ -10,16 +15,17 @@ namespace PomodoroInAction.Controllers
     [ApiController]
     public class AppUsersController : ControllerBase
     {
-        private UserManager<AppUser> _userManager;
+        private readonly UserManager<AppUser> _userManager;
+        private readonly ApplicationSettings _appSettings;
 
-        public AppUsersController(UserManager<AppUser> userManager)
+        public AppUsersController(UserManager<AppUser> userManager, IOptions<ApplicationSettings> appSettings)
         {
             _userManager = userManager;
+            _appSettings = appSettings.Value;
         }
 
         [HttpPost]
         [Route("Register")]
-        // POST: /api/AppUsers/Register
         public async Task<IActionResult> Register(AppUserModel model)
         {
             AppUser applicationUser = new AppUser()
@@ -37,6 +43,65 @@ namespace PomodoroInAction.Controllers
             {
                 throw ex;
             }
+        }
+
+        [HttpPost]
+        [Route("Login")]
+        public async Task<IActionResult> Login(LoginModel model)
+        {
+            AppUser user = await _userManager.FindByEmailAsync(model.Email);
+
+            if (user == null)
+            {
+                return BadRequest(new { message = "Email is not found" });
+            }
+
+            if (!await _userManager.CheckPasswordAsync(user, model.Password))
+            {
+                return BadRequest(new { message = "Password is incorrect" });
+            }
+
+            return Ok( new { Token = this.GenerateToken(user) } );
+        }
+
+        private string GenerateToken(AppUser user)
+        {
+            // SecurityTokenDescriptor: Contains some information which used to create a security token.
+            SecurityTokenDescriptor tokenDescriptor = new SecurityTokenDescriptor()
+            {
+                IssuedAt = DateTime.UtcNow,
+                Expires = DateTime.UtcNow.AddMinutes(120),
+                SigningCredentials = GetSigninCredentials(),
+                // Inside subject we need to put claims associated with the user
+                Subject = new ClaimsIdentity(new Claim[] {
+                    new Claim("UserID", user.Id.ToString())
+                }),
+                Issuer = _appSettings.JWT_Issuer
+            };
+
+            // A SecurityTokenHandler designed for creating and validating JWTs.
+            JwtSecurityTokenHandler _tokenHandler = new JwtSecurityTokenHandler();
+
+            // Generate jwt token
+            SecurityToken securityToken = _tokenHandler.CreateToken(tokenDescriptor);
+
+            // Serialize toket to "Compact Serialization Format"
+            return _tokenHandler.WriteToken(securityToken);
+        }
+
+        private SigningCredentials GetSigninCredentials()
+        {
+            // security key from safe vault
+            string _securityKey = _appSettings.JWT_Secret;
+
+            // symmetric key generated based on security key
+            // #TODO why exactly do we need this? GOOGLE IT
+            SymmetricSecurityKey _symmetricSecurityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_securityKey));
+
+            // SigningCredentials: Represents the cryptographic key and security algorithms that are used to generate a digital signature.
+            SigningCredentials _signingCredentials = new SigningCredentials(_symmetricSecurityKey, SecurityAlgorithms.HmacSha256Signature);
+
+            return _signingCredentials;
         }
     }
 }
---
+++ PomodoroInAction/Models/LoginModel.cs
@@ -0,0 +1,8 @@
+﻿namespace PomodoroInAction.Models
+{
+    public class LoginModel
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
